32277aa1c9358966d1dd1101a5dff066d8e9a316
Jorge <Jorge@JotaQuery.home>
2015-11-05 16:48:13 +0100

    Cambio sugerido por Nina

--- Boletin7Ej7/src/boletin7ej7/Boletin7Ej7.java
+++ Boletin7Ej7/src/boletin7ej7/Boletin7Ej7.java
@@ -15,42 +15,34 @@ public class Boletin7Ej7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        boolean cuadrado=false;
-        boolean triangulo=false;
-        boolean circulo = false;
         boolean opcionNoElegida = false;
         do{
         String res = JOptionPane.showInputDialog(null, "MENU\nElija una opción:\n1)Cadrado\n2)Triángulo\n3)Circulo");
         switch(Integer.parseInt(res)){
-            case 1:cuadrado=true;
+            case 1:
                 JOptionPane.showMessageDialog(null,"Ha elegido el cuadrado");
+                Cuadrado ejemplo = new Cuadrado();
+                ejemplo.introducirLado();
+                ejemplo.calcularArea();
                 break;
-            case 2:triangulo=true;
+            case 2:
                 JOptionPane.showMessageDialog(null,"Ha elegido el triangulo");
+                Triangulo ejemplo2 = new Triangulo();
+                ejemplo2.introducirBaseAltura();
+                ejemplo2.calcularArea();
                 break;
-            case 3:circulo=true;
+            case 3:
                 JOptionPane.showMessageDialog(null,"Ha elegido el circulo");
+                Circulo ejemplo3 = new Circulo();
+                ejemplo3.introducirRadio();
+                ejemplo3.calcularArea();
                 break;
             default:JOptionPane.showMessageDialog(null,"No ha elegido una opcion correcta");
                 opcionNoElegida = true;
         }
         }while(opcionNoElegida);
         
-        if(cuadrado){
-            Cuadrado ejemplo = new Cuadrado();
-            ejemplo.introducirLado();
-            ejemplo.calcularArea();
-        }
-        if(triangulo){
-            Triangulo ejemplo2 = new Triangulo();
-            ejemplo2.introducirBaseAltura();
-            ejemplo2.calcularArea();
-        }
-        if(circulo){
-            Circulo ejemplo3 = new Circulo();
-            ejemplo3.introducirRadio();
-            ejemplo3.calcularArea();
-        }
+        
         
     }
     
